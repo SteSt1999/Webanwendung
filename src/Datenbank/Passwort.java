b2ef554ad9090dd2ae42d345bf2449d80dc6f01e
stump <stump-stefan@t-online.de>
2018-10-24 09:00:18 +0200

    Passwort.java korrigiert

--- src/Datenbank/Passwort.java
+++ src/Datenbank/Passwort.java
@@ -1,9 +1,5 @@
 package Datenbank;
 
 public class Passwort {
-    final static String dbPasswort = "AT2867714";
-
-    public static String getDbPasswort() {
-        return dbPasswort;
-    }
+    final static String dbPasswort = "...";
 }
