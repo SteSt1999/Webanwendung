e4406a49a320e633d1744174e060b16b9d5fb138
stump <stump-stefan@t-online.de>
2018-10-24 14:45:25 +0200

    Fehler behoben

--- src/Logik/Umwandlung.java
+++ src/Logik/Umwandlung.java
@@ -4,18 +4,23 @@ public class Umwandlung {
     final private static long MAXIMUM = 10_000_000; //100.000 €
 
     public static long stringToLong(String betrag) {
+        System.out.println(betrag);
+
         String[] split = betrag.split("\\.");
         if (split.length > 2 || (split.length == 2 && split[1].length() > 2)) {
             throw new NumberFormatException();
         }
-        long n = 0;
-        n = Long.parseLong(betrag);
+        if (split.length == 2) {
+            betrag = split[0] + split[1];
+        } else {
+            betrag += "00";
+        }
 
+        long n = Long.parseLong(betrag);
         if (n > MAXIMUM || n <= 0) {
             throw new NumberFormatException();
         }
 
-        //Cent --> Euro
-        return n * 100;
+        return n;
     }
 }
